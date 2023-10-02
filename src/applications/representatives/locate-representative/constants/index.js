57b5936f1ba8f191461cac676a684b1d513d2b70
Colin O'Sullivan <143013011+cosu419@users.noreply.github.com>
2023-10-02 15:12:22 -0400

    Removed old constants, added RepresentativeType

--- src/applications/representatives/locate-representative/constants/index.js
+++ src/applications/representatives/locate-representative/constants/index.js
@@ -1,31 +1,11 @@
 /**
  * Enum for the type attribute of a Facility/Provider search result
  */
-export const LocationType = {
+export const RepresentativeType = {
   NONE: '',
-  VA_FACILITIES: 'va_facilities',
-  CC_PROVIDER: 'provider',
-  // Subtypes of VA_FACILITIES
-  HEALTH: 'health',
-  BENEFITS: 'benefits',
-  CEMETARY: 'cemetery',
-  VET_CENTER: 'vet_center',
-  URGENT_CARE: 'urgent_care',
-  URGENT_CARE_PHARMACIES: 'pharmacy',
-  EMERGENCY_CARE: 'emergency_care',
-};
-
-/**
- * Enum for the various Facility Types (inside the `attributes` object of a result)
- */
-export const FacilityType = {
-  VA_HEALTH_FACILITY: 'va_health_facility',
-  VA_CEMETARY: 'va_cemetery',
-  VA_BENEFITS_FACILITY: 'va_benefits_facility',
-  VET_CENTER: 'vet_center',
-  URGENT_CARE: 'urgent_care',
-  URGENT_CARE_PHARMACIES: 'pharmacy',
-  EMERGENCY_CARE: 'emergency_care',
+  VETERAN_SERVICE_ORGANIZATION: 'vso',
+  ATTORNEY: 'attorney',
+  CLAIMS_AGENT: 'claims_agent',
 };
 
 /**
@@ -33,27 +13,10 @@ export const FacilityType = {
  * Location types mapped to the filename prefix for the png/svg.
  */
 export const PinNames = {
-  [FacilityType.VA_HEALTH_FACILITY]: 'health',
-  [FacilityType.VA_CEMETARY]: 'cemetery',
-  [FacilityType.VA_BENEFITS_FACILITY]: 'benefits',
-  [FacilityType.VET_CENTER]: 'vet-centers',
-  [FacilityType.URGENT_CARE]: 'health',
-  [LocationType.CC_PROVIDER]: 'cc-provider',
-  [LocationType.URGENT_CARE_PHARMACIES]: 'cc-provider',
+  [RepresentativeType.VETERAN_SERVICE_ORGANIZATION]: 'vso',
+  [RepresentativeType.ATTORNEY]: 'attorney',
+  [RepresentativeType.CLAIMS_AGENT]: 'claims_agent',
 };
-
-/**
- * Defines the options available for the Location Type Dropdown
- */
-export const LOCATION_OPTIONS = [
-  LocationType.HEALTH,
-  LocationType.URGENT_CARE,
-  LocationType.CC_PROVIDER,
-  LocationType.BENEFITS,
-  LocationType.CEMETARY,
-  LocationType.VET_CENTER,
-  LocationType.EMERGENCY_CARE,
-];
 
 /**
  * Defines the ± change in bounding box size for the map when changing zoom
@@ -77,39 +40,12 @@ export const OperatingStatus = {
 };
 
 /**
- * Service type "Clinic/Center - Urgent Care" code
- */
-export const CLINIC_URGENTCARE_SERVICE = '261QU0200X';
-
-/**
- * Service type "Dentist - Orofacial Pain" code
- */
-export const DENTAL_OROFACIAL_PAIN_SERVICE = '1223X2210X';
-
-/**
- * Service type "Pharmacy - Community/Retail Pharmacy" code
- */
-export const PHARMACY_RETAIL_SERVICE = '3336C0003X';
-
-/**
- * Emergency Care Services codes
- */
-export const EMERGENCY_CARE_SERVICES = [
-  '261QE0002X',
-  '282N00000X',
-  '282NC0060X',
-  '282NR1301X',
-  '282NW0100X',
-];
-
-/**
  * Error Messages
  */
 export const Error = {
-  DEFAULT:
-    'We’re sorry. Something went wrong on our end. Please refresh this page or try again later.',
+  DEFAULT: `Something went wrong on our end. Please refresh this page or try again later.`,
   LOCATION:
-    'Something’s not quite right. Please enter a valid or different location and try your search again.',
+    'Something is not quite right. Please enter a valid or different location and try your search again.',
 };
 
 /**
@@ -144,5 +80,3 @@ export const MAX_SEARCH_AREA = 500;
  * Min radius search area in miles
  */
 export const MIN_RADIUS = 10;
-
-export const Covid19Vaccine = 'Covid19Vaccine';
