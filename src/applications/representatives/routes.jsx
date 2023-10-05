0ac6f627368d00e727157fabf00af1f109dd5195
Colin O'Sullivan <143013011+cosu419@users.noreply.github.com>
2023-10-05 14:15:47 -0400

    Adding routing for /form and /search

--- src/applications/representatives/routes.jsx
+++ src/applications/representatives/routes.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
-import { Route } from 'react-router';
-import App from './containers/App.jsx';
+// import React from 'react';
+// import { Route } from 'react-router';
+// import App from './containers/App.jsx';
 
-const routes = <Route path="/" component={App} />;
+// const routes = <Route path="/" component={App} />;
+
+// export default routes;
+
+import App from './containers/App';
+import Form from './containers/Form';
+import Search from './containers/Search';
+
+const routes = [
+  {
+    path: '/',
+    component: App,
+  },
+  {
+    path: '/search',
+    component: Search,
+  },
+  {
+    path: '/form',
+    component: Form,
+  },
+];
 
 export default routes;
